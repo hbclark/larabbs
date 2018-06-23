97f726003711f2cd02065e081fdb844572c7578c
vagrant <vagrant@homestead>
2018-06-23 08:15:02 +0000

    Simple layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            Design BY <a href="//hbclark.github.io" target="_blank">Clark He</a>
+        </p>
+        <p class="pull-right"><a href="mailto:arcaneade@gmail.com">Contact Us</a></p>
+    </div>
+</footer>
---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,34 @@
+<nav class="navbar navbar-default navbar-static-top">
+    <div class="container">
+        <div class="navbar-header">
+
+            <!-- Collapsed Hamburger -->
+            <button tyep="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                <span class="sr-only">Toggle Navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+
+            <!-- Branding Image -->
+            <a class="navbar-brand" href="{{url('/')}}">
+                Larabbs
+            </a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="app-navbar-collapse">
+            <!-- left side of navbar -->
+            <ul class="nav navbar-nav">
+
+            </ul>
+
+
+            <!-- Right side of Navbar -->
+            <ul class="nav navbar-nav navbar-right">
+                <!-- Authentication Links -->
+                <li><a href="#">Sign In</a></li>
+                <li><a href="#">Sign Up</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
