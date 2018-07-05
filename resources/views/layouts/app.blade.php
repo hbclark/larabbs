a64c6c603ca875e5a1a359041f1a7b5f5111701c
vagrant <vagrant@homestead>
2018-07-05 07:39:23 +0000

    minor patch

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,15 +6,6 @@
     <meta name="viewport" content="width=device-width,initial-scale=1">
 
     <!-- CSRF Token -->
-
-    <meta name="csrf-token" content="{{csrf_token()}}"
-
-          <title>@yield('title','Larabbs')</title>
-
-
-</head>
-
-=======
     <meta name="csrf-token" content="{{csrf_token()}}">
 
     <title>@yield('title','larabbs')Larabbs</title>
