5c02f37de93073f95654433d4630b22dc4194355
ClarkHe <arcaneade@gmail.com>
2018-07-19 11:42:17 +0000

    Add user policy

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -55,12 +55,11 @@
                             <li>
                                 <a href="{{route('logout')}}"
                                    onclick="event.preventDefault();
-                                            document.getElementById('logout-form').submit();">
+                                       document.getElementById('logout-form').submit()">
                                     Logout
-
                                 </a>
 
-                                <form id="logout-from" action="{{route('logout')}}" method="POST" style="display:none;">
+                                <form id="logout-form" action="{{route('logout')}}" method="POST" style="display:none;">
                                     {{csrf_field()}}
                                 </form>
                             </li>
