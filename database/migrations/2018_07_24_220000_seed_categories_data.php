bc20b869a4da5c53da7530b24c7e8712aa4ac06c
ClarkHe <arcaneade@gmail.com>
2018-07-24 12:06:01 +0000

    Create categories

---
+++ database/migrations/2018_07_24_220000_seed_categories_data.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class SeedCategoriesData extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $categories = [
+            [
+                'name'        =>'share',
+                'description' =>"Share invention",
+            ],
+            [
+              'name'          =>'tutorial',
+              'description'   =>'share',
+            ],
+        ];
+
+        DB::table('categories')->insert($categories);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::table('categories') ->truncate();
+    }
+}
